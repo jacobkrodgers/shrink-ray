c8392bf4990d5de5377f9fb642337f1ffdaddad3
jacobkrodgers <jacob.rodgers2@smail.astate.edu>
2023-03-29 02:52:58 -0500

    stub endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
-import './config'; // Load environment variables
-import 'express-async-errors'; // Enable default error handling for async errors
+import './config';
+import 'express-async-errors';
+import express, { Express } from 'express';
+import { notImplemented } from './controllers/NotImplementedController';
+
+const app: Express = express();
+app.use(express.json());
+const { PORT } = process.env;
+
+app.get('/api/users/:targetUserId/links', notImplemented);
+app.post('/api/links', notImplemented);
+app.delete('/api/users/:targetUserId/links/:targetLinkId', notImplemented);
+app.get('/:targetLinkId', notImplemented)
+app.post('/api/users', notImplemented);
+app.post('/api/login', notImplemented);
+
+app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}`);
+});
