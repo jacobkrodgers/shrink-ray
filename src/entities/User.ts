b7b649498db7c3a62866717a4b9215eb5bd7db44
jacobkrodgers <jacob.rodgers2@smail.astate.edu>
2023-03-29 03:00:15 -0500

    Create User Entity

---
+++ src/entities/User.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    userId: string;
+
+    @Column({ unique: true })
+    username: string;
+
+    @Column({ unique: true })
+    passwordHash: string;
+
+    @Column({ default: false })
+    isPro: boolean;
+
+    @Column({ default: false })
+    isAdmin: boolean;
+
+}
