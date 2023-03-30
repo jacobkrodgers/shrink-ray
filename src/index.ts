3562ca526ed44333560145b1901bee06b5a80207
jacobkrodgers <jacob.rodgers2@smail.astate.edu>
2023-03-29 22:54:49 -0500

    Configure Express Session

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
 import './config';
 import 'express-async-errors';
 import express, { Express } from 'express';
+import session from 'express-session';
+import connectSqlite3 from 'connect-sqlite3';
 import { notImplemented } from './controllers/NotImplementedController';
 
 const app: Express = express();
+const { PORT, COOKIE_SECRET } = process.env;
+const SQLiteStore = connectSqlite3(session);
+
+app.use(
+    session({
+        store: new SQLiteStore({ db: 'sessions.sqlite', }),
+        secret: COOKIE_SECRET,
+        cookie: { maxAge: 8 * 60 * 60 * 1000 }, // 8 hours 
+        name: 'session',
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+
 app.use(express.json());
-const { PORT } = process.env;
+
 
 app.get('/api/users/:targetUserId/links', notImplemented);
 app.post('/api/links', notImplemented);
