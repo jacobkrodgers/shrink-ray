53bb6874d6dfcd5d09f2827d1d180deb9606d71a
jacobkrodgers <jacob.rodgers2@smail.astate.edu>
2023-03-29 23:18:59 -0500

    Create User Model

---
+++ src/models/UserModel.ts
@@ -0,0 +1,26 @@
+import { AppDataSource } from '../dataSource';
+import { User } from '../entities/User';
+
+const userRepository = AppDataSource.getRepository(User);
+
+async function getUserByUsername(username: string): Promise<User | null> {
+    const user = await userRepository
+        .createQueryBuilder('user')
+        .leftJoinAndSelect('user.links', 'links')
+        .where('user.username = :username', { username })
+        .getOne();
+
+    return user;
+}
+
+async function addNewUser(username: string, passwordHash: string): Promise<User | null> {
+    let newUser = new User();
+    newUser.username = username;
+    newUser.passwordHash = passwordHash;
+
+    newUser = await userRepository.save(newUser);
+
+    return newUser;
+}
+
+export { getUserByUsername, addNewUser }
